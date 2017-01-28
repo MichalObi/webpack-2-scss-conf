0a846bfee11268c412730820a28289c3ca659194
MichalObi <ndwz123@gmail.com>
2017-01-28 17:44:45 +0100

    reindent / readme.md

---
+++ README.md
@@ -0,0 +1,18 @@
+Install: 
+
+npm i (-g) webpack@2 webpack-dev-server@2
+
+Loader to install:
+
+npm install css-loader - handle css
+
+npm install style-loader  - add css to DOM ( <style> tag injection )
+
+npm install sass-loader node-sass handle scss
+
+npm install extract-text-webpack-plugin@2.0.0-beta.5 - css in separate file
+
+To run 
+
+webpack (-p)
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-		  test: /\.(sass|scss)$/, 
-		  loader: ExtractTextPlugin.extract({
-		    fallbackLoader: "style-loader",
-		    loader: "css-loader!sass-loader",
-		  }),
-      } 
-    ]
-  },
-  plugins: [
-  	new ExtractTextPlugin("styles.css"),
-  ]
+	context: path.resolve(__dirname, './src'),
+	entry: {
+		app: './app.js',
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+		{
+			test: /\.(sass|scss)$/, 
+			loader: ExtractTextPlugin.extract({
+				fallbackLoader: "style-loader",
+				loader: "css-loader!sass-loader",
+			}),
+		} 
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("styles.css"),
+	]
 };
